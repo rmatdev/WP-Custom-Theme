21d862bf00462c76623477465e106683a546a816
rmatdev <matijevic.dev@gmail.com>
2023-02-16 11:12:54 +0100

    WP Custom Theme

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Main template file.
+ * 
+ * @package Wp Custom Theme
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prva WP Custom Tema</title>
+ </head>
+ <body>
+    Hello WordPress
+ </body>
+ </html>
